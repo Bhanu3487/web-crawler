c3dc76fbbd23976066171db9d492f0f9375071dd
Bhanu3487 <bhanu.repalle@iitgn.ac.in>
2025-01-21 20:03:34 +0530

    add getURLsFromHTML and tests

--- crawl.js
+++ crawl.js
@@ -1,4 +1,31 @@
+const {JSDOM} = require('jsdom');
 
+function getURLsFromHTML(htmlBody, baseURL){
+    // htmlBody: HTML of the page; baseURL: URL of the website that we are in the process of crawling
+    // returns urls: arr of strs representing URLs
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const linkElements = dom.window.document.querySelectorAll('a');
+    for (const linkElement of linkElements){
+        console.log(linkElement.href)
+        if(linkElement.href.slice(0,1) === '/'){
+            try{
+                const urlObj = new URL(`${baseURL}${linkElement.href}`);
+                urls.push(urlObj.href);
+            }catch(err){
+                console.log(`error ith relative URL: ${err.message}`);
+            }
+        }else{
+            try{
+                const urlObj = new URL(linkElement.href);
+                urls.push(urlObj.href);
+            }catch(err){
+                console.log(`error ith relative URL: ${err.message}`);
+            }
+        }
+    }
+    return urls
+}
 
 function normalizeURL(urlString){
     const urlObj = new URL(urlString);
@@ -10,5 +37,6 @@ function normalizeURL(urlString){
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
